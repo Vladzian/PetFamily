13d119858b3a6736905de419163864f9d7b13475
Владислав <vladzian@gmail.com>
2024-12-07 19:55:01 +0400

    Сохранение для перехода на другой ПК

--- PetFamily.Backend/src/PetFamily.Domain/Pet.cs
+++ PetFamily.Backend/src/PetFamily.Domain/Pet.cs
@@ -1,39 +1,42 @@
-﻿namespace PetFamily.Domain
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace PetFamily.Domain
 {
     public class Pet
     {
-        /*Нужно создать доменную богатую модель домашнего животного - Pet со следующими полями:
-
-1. Id   
-2. Кличка
-3. Вид(например - собака, кошка)
-4. Общее описание
-5. Порода
-6. Окрас
-7. Информация о здоровье питомца
-8. Адрес, где находится питомец
-9. Вес
-10. Рост
-11. Номер телефона для связи с владельцем
-12. Кастрирован или нет
-13. Дата рождения
-14. Вакцинирован или нет
-15. Статус помощи - Нуждается в помощи/Ищет дом/Нашел дом
-16. Реквизиты для помощи(у каждого реквизита будет название и описание, как сделать перевод), поэтому нужно сделать отдельный класс для реквизита.
-17. Дата создания*/
-
         public Guid Id { get; set; }
         public string ByName { get; set; } = null!;
         public string AnimalType { get; set; } = null!; //enum?
-        public string Description { get; set;} = null!;
-        public string Breed { get; set;} = null!;
-        public string Color { get; set;} = null!;
-        public string PetHealthInfo { get; set;} = null!;
-        public IAddress Address { get; set;} = null!; //VO
-      
+        public string Description { get; set; } = null!;
+        public string Breed { get; set; } = null!;
+        public string Color { get; set; } = null!;
+        public string PetHealthInfo { get; set; } = null!;
+        public IAddress Address { get; set; } = null!; //VO
+        public float  Weight { get; set; }
+        public float Height { get; set; }
+        public string OwnerPhoneNumber { get; set; }
+        public bool IsNeutered { get; set; }
+        public Date DateOfBirth { get; set; }
+        public bool IsVaccinated { get; set; }
+        public HelpStatus HelpStatus { get; set; }
+        public List<PropForHelp> PropsForHelp { get; set; }
+        public Date CreationDate { get; set; }
+
+
     }
 
-    public class PetAddress:IAddress
+    public class PropForHelp
+    {
+    }
+
+    public enum HelpStatus
+    {
+        NeedsHelp = 0,
+        LookingForAHome = 1,
+        FoundAHome = 2
+    }
+
+    public class PetAddress : IAddress
     {
     }
 
