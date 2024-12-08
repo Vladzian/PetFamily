7d929adf617f0a523387a7351aff274abe9e41df
vladzian <vladzian@gmail.com>
2024-12-08 19:12:29 +0400

    Добавлена богатая модель Pet. Добавлен класс PetId Добавлен класс RequisiteForHelp - реквизиты Добавлены классы Entity (немного вперед убежал) и Address

--- PetFamily.Backend/src/PetFamily.API/Program.cs
+++ PetFamily.Backend/src/PetFamily.API/Program.cs
@@ -1,8 +1,21 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using PetFamily.Domain;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
 app.Run();
